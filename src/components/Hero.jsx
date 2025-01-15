f4a3b0c01b9899ecd2fd97cf90cefa7fad2e97ae
rivazahra <riva.zahra88@gmail.com>
2025-01-16 01:19:44 +0700

    Add cv pdf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
           <h2 className='headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch]mt-5 mb-8 lg:mb-10'> Building Scalable Modern Websites for the Future
           </h2>
           <div className=" flex items-center gap-3 ">
-           <ButtonPrimary label="Download CV" href="" icon="download"/>
+           <ButtonPrimary label="Download CV" href="/cv.pdf" target="_blank" icon="download"/>
             <ButtonOutline href="#about" label="Scroll down"
             icon="arrow_downward"/>
           </div>
